a6c4423a57577bccc49e74eef4c4c58dab3f8c0d
unengine <unengine@gmail.com>
2021-07-13 17:31:59 +0900

    컨트롤러 일부 구현

--- Stellarium.Renderer/Assets/_Scripts/NetworkPoller.cs
+++ Stellarium.Renderer/Assets/_Scripts/NetworkPoller.cs
@@ -1,48 +1,52 @@
-﻿using System.Collections;
+using System.Collections;
 using UniRx;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class NetworkPoller : MonoBehaviour
+namespace Stellarium.Renderer.Networking
 {
-    private Coroutine _pollCoroutine = null;
-    private const float DELAY = 3f;
-    private readonly string urlFormat = "https://jsonplaceholder.typicode.com/posts/{0}";
-    public Subject<string> postSubject = new Subject<string>();
-
-    public void StartPolling()
+    public class NetworkPoller : MonoBehaviour
     {
-        _pollCoroutine = StartCoroutine(CoPoll());
-    }
+        private Coroutine _pollCoroutine = null;
+        private const float DELAY = 3f;
+        private readonly string urlFormat = "https://jsonplaceholder.typicode.com/posts/{0}";
+        public Subject<string> postSubject = new Subject<string>();
 
-    public void StopPolling()
-    {
-        StopCoroutine(_pollCoroutine);
-        _pollCoroutine = null;
-    }
-
-    private IEnumerator CoPoll()
-    {
-        var count = 1;
-        while (gameObject.activeSelf)
+        public void StartPolling()
         {
-            var url = string.Format(urlFormat, count);
-            var www = UnityWebRequest.Get(url);
-            yield return www.SendWebRequest();
+            _pollCoroutine = StartCoroutine(CoPoll());
+        }
 
-            if (www.error is null)
-            {
-                var result = www.downloadHandler.text;
-                postSubject.OnNext(result);
-            }
-            else
-            {
-                Debug.LogError("Error");
-                postSubject.OnNext(null);
-            }
+        public void StopPolling()
+        {
+            StopCoroutine(_pollCoroutine);
+            _pollCoroutine = null;
+        }
 
-            ++count;
-            yield return new WaitForSecondsRealtime(DELAY);
+        private IEnumerator CoPoll()
+        {
+            var count = 1;
+            while (gameObject.activeSelf)
+            {
+                var url = string.Format(urlFormat, count);
+                var www = UnityWebRequest.Get(url);
+                yield return www.SendWebRequest();
+
+                if (www.error is null)
+                {
+                    var result = www.downloadHandler.text;
+                    postSubject.OnNext(result);
+                }
+                else
+                {
+                    Debug.LogError("Error");
+                    postSubject.OnNext(null);
+                }
+
+                ++count;
+                yield return new WaitForSecondsRealtime(DELAY);
+            }
         }
     }
 }
+
