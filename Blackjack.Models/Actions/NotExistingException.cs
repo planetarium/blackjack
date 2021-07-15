78f0f79db9b2cc6ae5069f65e3e0db246c684f15
marshall <kfangw@gmail.com>
2021-07-15 18:20:54 +0900

    Fill APIs

---
+++ Blackjack.Models/Actions/NotExistingException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.Serialization;
+using Libplanet;
+
+namespace Blackjack.Models.Actions
+{
+    public class NotExistingException : Exception
+    {
+        public Address Address { get; private set; }
+
+        public NotExistingException(Address address)
+        {
+            Address = address;
+        }
+
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+
+            info.AddValue(nameof(Address), Address);
+ }
+    }
+}
---
+++ Blackjack.Models/Actions/StayAction.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using Bencodex.Types;
+using Blackjack.Models.States;
+using Libplanet;
+using Libplanet.Action;
+using Libplanet.Blockchain.Renderers.Debug;
+
+namespace Blackjack.Models.Actions
+{
+    [Serializable]
+    [ActionType("start")]
+    public class StayAction : BaseAction
+    {
+        private Address _address;
+
+        private long _stay;
+
+        public StayAction()
+        {
+        }
+
+        public StayAction(Address address, long stay)
+        {
+            _address = address;
+            _stay = stay;
+        }
+
+        public override IAccountStateDelta Execute(IActionContext context)
+        {
+            if (context.Rehearsal)
+            {
+                return context.PreviousStates.SetState(_address, MarkChanged);
+            }
+            IAccountStateDelta state = context.PreviousStates;
+            if (!_address.Equals(context.Signer))
+            {
+                throw new PermissionException(_address, context.Signer);
+            }
+
+            var stateValue = state.GetState(_address);
+
+            if (!(stateValue is { }))
+            {
+                throw new NotExistingException(_address);
+            }
+
+            var accountState = new BlackJackAccountState((Dictionary)stateValue);
+
+            if (accountState.Status != BlackJackAccountState.AccountStatus.InGame)
+            {
+                throw new InvalidOperationException();
+            }
+
+            accountState.MoneyEarned +=
+                BlackJack(accountState.RandomSeed, _stay, accountState.MoneyCumulated);
+
+            accountState.MoneyCumulated = 0;
+            accountState.Status = BlackJackAccountState.AccountStatus.StandBy;
+            accountState.CumulatingSince = context.BlockIndex;
+            return state.SetState(_address, accountState.Serialize());
+        }
+
+        public override void LoadPlainValue(IValue plainValue)
+        {
+            var asDict = (Dictionary) plainValue;
+            _address = asDict[nameof(_address)].ToAddress();
+        }
+
+        public override IValue PlainValue =>
+            new Dictionary(
+                new[]
+                {
+                    new KeyValuePair<IKey, IValue>((Text)nameof(_address), _address.Serialize())
+                }
+            );
+
+        private long BlackJack(long randomSeed, long stay, long money)
+        {
+            //TODO: call blackjack JS
+            return 0;
+        }
+    }
+}
